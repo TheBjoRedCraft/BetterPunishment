ca4e47a339ef6356e01c0ab9d574427308ea1c40
Red <red.dev@hexle.at>
2024-02-14 19:04:16 +0100

    first commit

---
+++ src/main/java/dev/thebjoredcraft/betterpunishment/BetterPunishment.java
@@ -0,0 +1,17 @@
+package dev.thebjoredcraft.betterpunishment;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterPunishment extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
